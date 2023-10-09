1831e8d00dacb04c150057a2505c3fa1128b3b73
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-10-09 11:42:19 -0600

    Actualización de rutas y carga perezosa de rutas

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePagesComponent } from './pages/home-pages/home-pages.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { ContactPagesComponent } from './pages/contact-pages/contact-pages.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePagesComponent,
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPagesComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
+  ],
+  exports: [
+    HomePagesComponent,
+    AboutPageComponent,
+    ContactPagesComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
