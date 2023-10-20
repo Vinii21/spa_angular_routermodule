0c4abac8f3e73d200fbdf8e79f5c71e00c89955b
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-10-19 22:45:45 -0600

    Persistencia de datos

--- src/app/countries/components/contry-table/contry-table.component.ts
+++ src/app/countries/components/contry-table/contry-table.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
 
 
+
 @Component({
   selector: 'countries-contry-table',
   templateUrl: './contry-table.component.html',
--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,8 +1,7 @@
-import { Component } from '@angular/core';
-import { Country } from '../../interfaces/country';
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/countries.service';
-
-type Region = 'Africa'| 'Americas'| 'Asia'| 'Europe'| 'Oceania';
+import { Region } from '../../interfaces/regin.type';
 
 @Component({
   selector: 'app-by-region-page',
@@ -10,13 +9,18 @@ type Region = 'Africa'| 'Americas'| 'Asia'| 'Europe'| 'Oceania';
   styles: [
   ]
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
 
   public countries: Country[] = [];
   public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
   public selectedRegion?: Region;
 
   constructor( private countriesService: CountriesService ) {}
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byRegion.countries;
+    this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+  }
 
   searchByRegion( region: Region ):void  {
     this.selectedRegion = region;
